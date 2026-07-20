6a045d5a680a385543d4f525506bad1387f6d60f
Leandro De la cruz <leandrocitygear@icloud.com>
2026-07-20 15:38:13 -0400

    letter fix

--- src/app/contactPage.ts
+++ src/app/contactPage.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   <div class="h-50"></div>
 
     <div class="min-h-screen p-20">
-      <h1 class="text-6xl font-bold">contact</h1>
+      <h1 class="text-6xl font-bold">Contact</h1>
       <p class="mt-6 text-neutral-600">
         My contact info will be here.
       </p>
